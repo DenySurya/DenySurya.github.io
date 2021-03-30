3759884f8ff44860797db36b2571b777f5e420cc
Deny Surya <sgunawan640@gmail.com>
2021-03-30 18:16:17 +0800

    first commit

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css " href="{{asset('css/style.css')}}">
+
+
+    <title>WISATA BEDUGUL</title>
+</head>
+
+
+<body id="page-top">
+    <!-- NAVBAR -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-success fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">WISATA BEDUGUL</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">HOME<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="deret/fibbonacci.php">ABOUT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="deret/aritmatika.php">GALERY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="html/boking.php">BOOKING</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- NAVBAR -->
+
+    <!-- SLIDEBAR -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <!-- gambar1 -->
+                <img src="asset/gambar/10sb.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block text-center">
+                    <h1 class="display-4">WISATA BEDUGUL<br> <span class="font-weight-bold">BALI</span></h1>
+                    <p class="lead">nikmati suasana sejuk pegunungan</p>
+                </div>
+            </div>
+            <!-- gambar2 -->
+            <div class="carousel-item">
+                <img src="asset/gambar/7sb.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block text-right">
+                    <h1 class="display-5">BATURITI, TABANAN, BALI<br> <span class="font-weight-bold">PULAU DEWATA</span></h1>
+                    <p class="lead">nyaman berwisata bersama keluarga</p>
+                </div>
+            </div>
+            <!-- gambar3 -->
+            <div class="carousel-item">
+                <img src="asset/gambar/9sb.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block text-left">
+                    <h1 class="display-6">PEMANDANGAN DANAU<br> <span class="font-weight-bold">BERATAN</span></h1>
+                    <p class="lead">Dipenuhi dengan pemandangan yang memanjakan mata</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- SLIDEBAR -->
+
+    <!-- ABOUT -->
+    <section class="about">
+        <div class="container">
+            <h2 class="tabout"> TENTANG BEDUGUL</h2>
+            <hr>
+            <div class="row">
+                <div class="col-md-6">
+                    <p> Kawasan Bedugul adalah salah satu daerah terkenal di pulau Dewata Bali. tempat ini merupakan tempat salah satu destinasi wisata terbaik yang ada di pulai Bali. setiap harinya banyak wisatawan domestik maupun mancanegara yang berkunjung ke tempat ini. </p>
+                </div>
+
+                <div class="col-md-6">
+                    <p> Di daerah ini terdapat sebuah danau yang begitu indah yaitu danau Beratan. danau ini adalah salah satu danau terindah yang ada di Indonesia. di Bedugul ini terdapat berbagai tempat wisata yaitu DTW Pura Ulun Danu Beratan, The Bloms Garden, Wisata Tepi Danau, Kebun Raya, dan yang lainnya</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ABOUT -->
+
+    <!-- GALERI -->
+    <section class="galeri">
+        <div class="container">
+            <h3> GALERY </h3>
+            <hr>
+            <P> Berikut adalah objek wisata yang ada di Bedugul</P>
+            <!-- gambar 1 -->
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="galeri-img">
+                        <a href="" class="thumbnail">
+                            <img src="asset/galeri/2gl.png">
+                        </a>
+                        <h4> Danau Beratan</h4>
+                        <p>Lake Beratan</p>
+                    </div>
+                </div>
+                <!-- gambar 2 -->
+                <div class="col-md-4">
+                    <div class="galeri-img">
+                        <a href="" class="thumbnail">
+                            <img src="asset/galeri/3gl.png">
+                        </a>
+                        <h4> Kebun Raya Bali</h4>
+                        <p>Bali Botanaical Garden </p>
+                    </div>
+                </div>
+                <!-- gambar 3 -->
+                <div class="col-md-4">
+                    <div class="galeri-img">
+                        <a href="" class="thumbnail">
+                            <img src="asset/galeri/1.gl.png">
+                        </a>
+                        <h4> Pura Ulun Danu Beratan</h4>
+                        <p>Ulun Danu Berata Tample</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </section>
+    <!-- GALERI -->
+
+    <!-- CONTACT US -->
+    <section class="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h4>Contact Us</h4>
+                    <hr>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-sm-4">
+                    <div class="card border-success mb-3 text-center">
+                        <div class="card-header">CONTACT US</div>
+                        <div class="card-body text-success">
+                            <h5 class="card-title">Hubungi Kami</h5>
+                            <p class="card-text">Jika ada yang diperlukan bisa hubungi kami</p>
+                        </div>
+                    </div>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <h3>LOCATION</h3>
+                        </li>
+                        <li class="list-group-item">Jln Denpasar Singaraja</li>
+                        <li class="list-group-item">Baturiti Tabanan</li>
+                        <li class="list-group-item">Bali</li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <form>
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control" id="nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="text" class="form-control" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="telp">No Telp:</label>
+                            <input type="text" class="form-control" id="telp">
+                        </div>
+                        <div class="form-group">
+                            <label for="pesan">Pesan:</label>
+                            <textarea name="pesan" id="pesan" class="form-control"></textarea>
+                        </div>
+                        <div class="from-group">
+                            <button type="button" class="btn btn-success">Kirim Pesan!</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+    </section>
+    <!-- CONTACT US -->
+
+
+    <!-- FOOTER -->
+    <footer class="bg-dark text-white">
+        <div class="container">
+            <div class="row pt-3">
+                <div class="col text-center">
+                    <p>Deny Surya | Copyright 2021</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- FOOTER -->
+
+    >
+    <!-- GALERY -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
